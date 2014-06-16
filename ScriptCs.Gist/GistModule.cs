038042f86c7ee17574679c2bc6f1655792c75336
Ryan Rousseau <ryan.rousseau@gmail.com>
2014-06-16 17:23:48 -0500

    Module, LineProcessor, nuspec
    Seems to be working.

---
+++ ScriptCs.Gist/GistModule.cs
@@ -0,0 +1,13 @@
+﻿using ScriptCs.Contracts;
+
+namespace ScriptCs.Gist
+{
+    [Module("gist", Extensions = "csx")]
+    public class GistModule : IModule
+    {
+        public void Initialize(IModuleConfiguration config)
+        {
+            config.LineProcessor<GistLineProcessor>();
+        }
+    }
+}
